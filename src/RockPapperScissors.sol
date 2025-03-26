741d664baa43d4b657785860e5467c65254ea3a1
Patrick keitam <patrick.keita@outlook.com>
2025-03-26 17:13:28 +0100

    cleanup VRF

--- src/RockPapperScissors.sol
+++ src/RockPapperScissors.sol
@@ -72,16 +72,6 @@ contract RockPapperScissors {
     mapping(address => bool) isInListOfPlayers;
     mapping(address => uint) placeOfPlayer;
 
-    // chainlink VRF variables
-    uint256 s_subscriptionId;
-    address vrfCoordinator = 0x9DdfaCa8183c41ad55329BdeeD9F6A8d53168B1B;
-    bytes32 s_keyHash =
-        0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae;
-    uint32 callbackGasLimit = 40000;
-    uint16 requestConfirmations = 3;
-    uint32 numWords = 1;
-    //
-
     event NewpPlayerEnteredTheGame(
         address indexed playerAddress,
         uint256 indexed playerBet,
